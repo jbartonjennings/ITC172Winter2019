6e16de09d0bac14fc4faa523247a37fa236df8ad
jbartonjennings <46457585+jbartonjennings@users.noreply.github.com>
2019-03-04 11:36:24 -0800

    Add files via upload

--- pythonclubproj/pythonclubproj/club/views.py
+++ pythonclubproj/pythonclubproj/club/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Resource
+from django.shortcuts import render, get_object_or_404
+from .models import Resource, Meeting
 
 # Create your views here.
 
@@ -10,3 +10,11 @@ def resource(request):
     resource_list=Resource.objects.all()
     return render(request, 'club/resource.html', {'resource_list':resource_list})
 
+def getmeetings(request):
+    meeting_list=Meeting.objects.all()
+    return render (request, 'club/meeting.html', {'meeting_list':meeting_list})    
+
+def meetingdetail(request, id):
+    detail=get_object_or_404(Meeting, pk=id)
+    context = {'detail': detail}
+    return render (request, 'club/details.html', context=context)
